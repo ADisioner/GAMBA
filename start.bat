1f7298afcb8d8ab26ec246e9108f274a4d6928ea
ADisioner <129493385+ADisioner@users.noreply.github.com>
2026-03-29 12:50:06 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -11,4 +11,4 @@ start "PMC-SERVER" /D "%~dp0server" cmd /c "npm install && npm run dev"
 :: Launch Frontend in CURRENT console (shows logs here)
 echo [2/2] Launching Frontend...
 cd /d "%~dp0casino"
-npm install && npm run dev
+npm install && npm run dev -- --host
